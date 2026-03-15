c7c73dae31cceedafe723da1f0966186b3932493
git-richardwu <richardwu720@gmail.com>
2026-03-15 17:39:35 -0400

    Add unsaved changes tracking and refactor structure for clarity

--- frontend/src/components/Glossary/EditChapter.tsx
+++ frontend/src/components/Glossary/EditChapter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import type { Character, Chapter } from '../../types';
 import styles from './EditChapter.module.css'
 import { RiEdit2Fill } from "react-icons/ri";
@@ -10,19 +10,102 @@ interface EditChapterProps {
     onSave: (updatedChapter: Chapter) => void;
     onCancel: () => void;
     onDelete: () => void;
+    isNew: boolean;
 }
 
-const EditChapter = ({ chapter, work_id, onSave, onCancel, onDelete }: EditChapterProps) => {
+interface UnsavedChapterChanges {
+    chapter_name: boolean;
+    characters: {
+        added: boolean;
+        removed: boolean;
+        modified: boolean;
+    }
+    hasChanges: boolean;
+    isNewChapter: boolean;
+}
+
+const EditChapter = ({ chapter, work_id, onSave, onCancel, onDelete, isNew }: EditChapterProps) => {
 
     const [editingChapter, setEditingChapter] = useState<Chapter>(chapter)
+    const [originalChapter, setOriginalChapter] = useState<Chapter>(chapter)
+    const [unsavedChanges, setUnsavedChanges] = useState<UnsavedChapterChanges>({
+        chapter_name: false,
+        characters: {
+            added: false,
+            removed: false,
+            modified: false,
+        },
+        hasChanges: isNew,
+        isNewChapter: isNew
+    })
 
     useEffect(() => {
         setEditingChapter(chapter);
-    }, [chapter])
+        setOriginalChapter(chapter);
+        setUnsavedChanges(prev => ({
+            ...prev,
+            hasChanges: isNew
+        }))
+    }, [chapter, isNew])
+
+    useEffect(() => {
+        if (isNew) {
+            setUnsavedChanges({
+                chapter_name: true,
+                characters: {
+                    added: true,
+                    removed: false,
+                    modified: false,
+                }, hasChanges: true,
+                isNewChapter: true
+            });
+            return;
+        }
+        const changes: UnsavedChapterChanges = {
+            chapter_name: editingChapter.chapter_name !== originalChapter.chapter_name,
+            characters: {
+                added: false,
+                removed: false,
+                modified: false,
+            },
+            hasChanges: false,
+            isNewChapter: false
+        }
+        const originalChars = originalChapter.characters || []
+        const editingChars = editingChapter.characters || []
+
+        if (editingChars.length > originalChars.length) {
+            changes.characters.added = true
+        }
+
+        if (editingChars.length < originalChars.length) {
+            changes.characters.removed = true
+        }
+        for (let i = 0; i < Math.min(editingChars.length, originalChars.length); i++) {
+            const editingChar = editingChars[i];
+            const originalChar = originalChars[i];
+            if (originalChar.name !== editingChar.name ||
+                originalChar.description !== editingChar.description ||
+                originalChar.central_character !== editingChar.central_character
+            ) {
+                changes.characters.modified = true;
+                break;
+            }
+        }
+        changes.hasChanges = changes.chapter_name || changes.characters.added || changes.characters.modified || changes.characters.removed;
+        setUnsavedChanges(changes);
+    }, [editingChapter, originalChapter, isNew])
 
     const handleSubmitChanges = (e: React.FormEvent) => {
         e.preventDefault();
         onSave(editingChapter);
+        if (isNew) {
+            setUnsavedChanges(prev => ({
+                ...prev,
+                isNewChapter: false,
+                hasChanges: false
+            }))
+        }
     }
 
     const handleChapterNameChange = (value: string) => {
@@ -33,16 +116,15 @@ const EditChapter = ({ chapter, work_id, onSave, onCancel, onDelete }: EditChapt
     }
 
     const handleCharacterChange = (index: number, field: keyof Character, value: string | boolean) => {
-            const updatedChapters = [...editingChapter.characters];
-            updatedChapters[index] = {
-                ...updatedChapters[index],
-                [field]: value
-            };
-
-            setEditingChapter({
-                ...editingChapter,
-                characters: updatedChapters
-            })
+        const updatedChapters = [...editingChapter.characters];
+        updatedChapters[index] = {
+            ...updatedChapters[index],
+            [field]: value
+        };
+        setEditingChapter({
+            ...editingChapter,
+            characters: updatedChapters
+        })
     }
 
     const handleRemoveCharacter = (charIndex: number) => {
@@ -55,40 +137,52 @@ const EditChapter = ({ chapter, work_id, onSave, onCancel, onDelete }: EditChapt
     const handleAddCharacter = () => {
         setEditingChapter({
             ...editingChapter,
-            characters:[...editingChapter.characters,
-                {name: "Character Name", description: "Placeholder", central_character: false}
+            characters: [...editingChapter.characters,
+            { name: "Character Name", description: "Placeholder", central_character: false }
             ]
         })
     }
 
+    const handleCancel = () => {
+        onCancel();
+        setEditingChapter(originalChapter);
+    }
 
     return (
         <form onSubmit={handleSubmitChanges} >
-            <div className={styles.parent}>
-                <div className={styles.chapter}>
+            <div className={styles.parentContainer}>
+                <button className={styles.editButton} type="submit">save</button>
+                <button className={styles.editButton} type="button" onClick={handleCancel}>cancel</button>
+                <hr />
+                {unsavedChanges.hasChanges && <h1>UNSAVED CHANGES</h1>}
+                <div className={styles.row}>
                     <input className={styles.chapterInput} value={editingChapter.chapter_name} onChange={(e) => handleChapterNameChange(e.target.value)} required />
-                    <button type="submit">SAVE</button>
-                    <button type="button" onClick={onDelete}>DELETE</button>
-                    <button type="button" onClick={onCancel}>CANCEL</button>
+                    <button type="button" onClick={onDelete}>delete chapter</button>
                 </div>
-                <div>
+                <div id="characterList">
                     {
                         editingChapter.characters.map((character: Character, index: number) => (
                             <div key={`${work_id}_${chapter.chapter_name}_${index}`}>
-
-                                <div className={styles.content}>
-                                    <div>
-                                        <input className={styles.name} value={character.name} onChange={(e) => handleCharacterChange(index, 'name', e.target.value)} required />
-                                        {/* <FaTrashAlt onClick={() => handleRemoveCharacter(index)} className={styles.icon} /> */}
-                                        <button type="button" onClick={() => handleRemoveCharacter(index)}>DELETE CHARACTER</button>
-                                    
+                                <div className={styles.row}>
+                                    <input className={styles.nameInput} value={character.name} onChange={(e) => handleCharacterChange(index, 'name', e.target.value)} required />
+                                    <div className={styles.centralToggle}>
+                                        <label>central character? </label>
+                                        <input id="toggle" type="checkbox" checked={character.central_character} onChange={(e) => handleCharacterChange(index, 'central_character', e.target.checked)} />
                                     </div>
+                                </div>
+                                <div className={styles.row}>
                                     <textarea className={styles.description} value={character.description} onChange={(e) => handleCharacterChange(index, 'description', e.target.value)} required />
                                 </div>
+                                <div className={styles.row}>
+                                    <button type="button" onClick={() => handleRemoveCharacter(index)}>delete character</button>
+                                </div>
+                                <hr />
                             </div>
                         ))
                     }
-                    <button type="button" onClick={handleAddCharacter}>ADD CHARACTER</button>
+                    <div className={styles.row}>
+                        <button type="button" className={styles.editButton} onClick={handleAddCharacter}>add character</button>
+                    </div>
 
                 </div>
             </div>
