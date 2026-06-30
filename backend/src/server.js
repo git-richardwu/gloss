2d5a0fb54c8debe3e3de9fedf6c68ec6262ef747
git-richardwu <richardwu720@gmail.com>
2026-06-30 17:48:37 -0400

    Revert CORS origin for dev

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ const http = require('http');
 const port = 3000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:9000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
