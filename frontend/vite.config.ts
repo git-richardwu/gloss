e311c91e41b372a8c88b74fd6f7b85a011ecc07e
git-richardwu <richardwu720@gmail.com>
2026-07-02 17:25:39 -0400

    Update vite.config.ts with allowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: [
+      'gloss-frontend-production.up.railway.app'
+    ]
+  },
   plugins: [
     react({
       babel: {
